b64e51f301304143cc553f797a4c9bb347e10a34
zhangyanfeng <my_163vipemail@163.com>
2023-11-27 16:39:54 +0800

    调整响应结构

--- app/api/raw-order/route.js
+++ app/api/raw-order/route.js
@@ -9,9 +9,9 @@ export async function GET(request) {
     const requestBusinessNo = searchParams.get('requestBusinessNo')
     const order = await RawOrder.findOne({requestBusinessNo})
     // console.log('debug--22', order);
-    return Response.json(order)
+    return Response.json({ code: '200', data: order, msg: '查询成功' })
   } catch (err) {
-    return Response.json({}, { status: 500, statusText: '数据库响应超时'})
+    return Response.json({ code: '202', data: {}, msg: '数据库响应超时' })
   }
 }
 
@@ -21,8 +21,8 @@ export async function POST(request) {
     const res = await request.json()
     const order = new RawOrder(res)
     await order.save()
-    return Response.json({msg: '入库成功'})
+    return Response.json({code: '200', data: {}, msg: '入库成功'})
   } catch (err) {
-    return Response.json({}, { status: 500, statusText: '数据库响应超时'})
+    return Response.json({code: '202', data: {}, msg: '数据库响应超时'})
   }
 }
