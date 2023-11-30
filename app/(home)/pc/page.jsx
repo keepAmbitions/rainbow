d126047a24cd3074af03b14f8e1a278075189e71
zhangyanfeng <my_163vipemail@163.com>
2023-11-30 20:24:41 +0800

    调整RawOrder接口字段&删除未使用包

--- app/(home)/pc/page.jsx
+++ app/(home)/pc/page.jsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import Paper from "@mui/material/Paper";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+
+import style from "./index.module.css";
 const WelcomeHome = () => {
   const [inValue, setInValue] = useState("");
   const [outValue, setOutValue] = useState("");
@@ -20,51 +22,51 @@ const WelcomeHome = () => {
 
   const getData = async () => {
     try {
-      const res = await fetch(`/api/raw-order?requestBusinessNo=${inValue}`)
-      
+      const res = await fetch(`/api/raw-order?requestBusinessNo=${inValue}`);
+
       if (!res.ok) {
         // This will activate the closest `error.js` Error Boundary
-        throw new Error('Failed to fetch data')
+        throw new Error("Failed to fetch data");
       }
-      const data = await res.json()
-      console.log('debug--11', data);
-      
-      setOutValue(data.requestBusinessNo)
-    } catch (err) {
-      
-    };
+      const data = await res.json();
+      console.log("debug--11", data);
+
+      setOutValue(data.requestBusinessNo);
+    } catch (err) {}
   };
   return (
-    <Paper
-      elevation={3}
-      sx={{ width: "100%", height: "120vh", bgcolor: "antiquewhite" }}
-    >
-      <TextField
-        id="outlined-basic"
-        size="small"
-        label="输入"
-        variant="outlined"
-        value={inValue}
-        onChange={(event) => {
-          setInValue(event.target.value);
-        }}
-      />
-      <Button variant="contained" onClick={postData}>
-        提交
-      </Button>
-      <TextField
-        id="outlined-basic"
-        size="small"
-        label="输出"
-        variant="outlined"
-        value={outValue}
-        onChange={(event) => {
-          setOutValue(event.target.value);
-        }}
-      />
-      <Button variant="contained" onClick={getData}>
-        查询
-      </Button>
+    <Paper elevation={3} sx={{ width: "100%", bgcolor: "antiquewhite" }}>
+      <Paper elevation={3} sx={{ height: "120vh" }}>
+        内容
+      </Paper>
+      <div className={style.stickyWrap}>
+        <TextField
+          id="outlined-basic"
+          size="small"
+          label="输入"
+          variant="outlined"
+          value={inValue}
+          onChange={(event) => {
+            setInValue(event.target.value);
+          }}
+        />
+        <Button variant="contained" onClick={postData}>
+          提交
+        </Button>
+        <TextField
+          id="outlined-basic"
+          size="small"
+          label="输出"
+          variant="outlined"
+          value={outValue}
+          onChange={(event) => {
+            setOutValue(event.target.value);
+          }}
+        />
+        <Button variant="contained" onClick={getData}>
+          查询
+        </Button>
+      </div>
     </Paper>
   );
 };
--- app/models/RawOrder.js
+++ app/models/RawOrder.js
@@ -1,19 +1,10 @@
 import mongoose from 'mongoose'
 import { baseSchema, option } from './help.js'
 const RawSubOrderSchema = mongoose.Schema({
-  requestBusinessNo: {
-    type: String,
-    // require: true
-  },
-  step: String,
-  itemName: String,
-  itemNo: String,
-  itemPrice: String,
-  discountPrice: String,
-  commissionDiscountPrice: String,
-  commission: String,
-  itemType: String,
-  remark: String,
+  step: String, // 阶段（只有履约订单才有值，微包必传）
+  itemPrice: String, // 商品/款项金额
+  commissionDiscountPrice: String, // 服务费优惠后金额
+  commission: String, // 服务费
 })
 const RawOrderSchema = mongoose.Schema({
   ...baseSchema,
@@ -23,31 +14,21 @@ const RawOrderSchema = mongoose.Schema({
   },
   name: String, // 订单描述
   totalAmount: String, // 项目总价
-  commission: String, // 平台服务费
-  discountAmount: String, // 订单优惠金额
+  totalCommission: String, // 平台总服务费
+  totalDiscountAmount: String, // 订单总优惠金额
   totalActualAmount: String, // 订单实付金额（最终要付的钱）
 
   sourceUserId: String, // 买家收货人/雇主用户编号
   sourceConsignee: String, // 买家收货人/雇主
-  sourcePhone: String, // 买方联系方式
+  vvIdBuyer: '', // 买方微微号
+  vvIdSeller: '', // 卖联微微号
   sourceType: String, // 买家身份（0：企业，1、个人
   targetSellerId: String, // 卖家/服务使用户编号
   targetSellerName: String, // 卖家收货人/服务商名称
   sellerType: String, // 卖家身份（0：企业，1、个人
-  targetPhone: String, // 卖联系方式
 
-  targetShopId: String, // 卖家店铺ID/服务商场所ID
-  contractId: String, // 合同编号
-  contractUrl: String, // 合同url
-  country: String, // 订单来源国家
-  remark: String, // 订单备注
-  paymentMethod: String, // 支付方式（0：app，1：pc）
   detailOrderCount: String, // 订单明细数量
   subOrder: [RawSubOrderSchema],
-  // author: {
-  //   type: mongoose.Types.ObjectId,
-  //   ref: 'User',
-  // },
 }, option)
 
 export default mongoose.model('RawOrder', RawOrderSchema)
