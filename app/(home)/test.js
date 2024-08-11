95ee7304e0881f84471edbec829b921672ae3b0d
zhangyanfeng <my_163vipemail@163.com>
2024-08-11 16:56:26 +0800

    暂存内容

---
+++ app/(home)/test.js
@@ -0,0 +1,22 @@
+const people = [
+    {name:'Daniel', age: 14, gender: 'MALE'}, 
+    {name:'Alice', age: 13, gender: 'FEMALE'}, 
+    {name:'Jack', age: 12, gender: 'MALE'}, 
+    {name:'Ivy', age: 15, gender: 'FEMALE'}, 
+  ]
+  
+  // 实现两个函数，函数的入参是数组
+  // 1. 求所有男性的年龄平均值 
+  
+  // 2. 根据年龄，返回从大到小排序的数组
+
+  function pj(arr) {
+    let arr2 = []
+    arr.forEach(item=>{
+        if (item.gender==='MALE') {
+            arr2.push(item.age)
+        }
+    })
+    return arr.reduce(sum, curr=>sum += curr)/ arr.length
+  }
+ console.log( pj(people));
